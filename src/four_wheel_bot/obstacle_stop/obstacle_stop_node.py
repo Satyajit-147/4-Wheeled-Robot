adc17e7e1f708ca06bbdc2662372e63b91fa8d01
Satyajit-147 <satyajitgr9@gmail.com>
2025-06-21 21:56:29 +0530

    Update obstacle_stop_node.py

--- src/four_wheel_bot/obstacle_stop/obstacle_stop_node.py
+++ src/four_wheel_bot/obstacle_stop/obstacle_stop_node.py
@@ -13,22 +13,18 @@ class ObstacleStopper(Node):
         # Subscriber: listens to LaserScan for obstacle detection
         self.scan_sub = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
 
-        # Subscribing to /cmd_vel 
-        self.cmd_vel = Twist()
-        self.cmd_vel_sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
-
-    def cmd_vel_callback(self, msg):
-        self.cmd_vel = msg  # Store latest command to forward if safe
-
     def scan_callback(self, msg):
         min_distance = min(msg.ranges)
         self.get_logger().info(f"Closest object: {min_distance:.2f} m")
 
+        cmd_vel = Twist()
         if min_distance < 0.5:
             self.get_logger().warn("Obstacle too close! Stopping robot.")
-            self.publisher.publish(Twist())  # Stop
+            cmd_vel.linear.x = 0.0
         else:
-            self.publisher.publish(self.cmd_vel)  # Forward latest cmd_vel if safe
+            cmd_vel.linear.x = 0.2  # move forward
+
+        self.publisher.publish(cmd_vel)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -38,4 +34,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
